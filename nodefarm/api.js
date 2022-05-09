aebc134c088358a8577861b6edf6c338827b8d2f
Abhyudaya Kandwal <abhyudayakandwal001@gmail.com>
2022-05-09 16:31:11 +0530

    update

--- nodefarm/api.js
+++ nodefarm/api.js
@@ -1,18 +1,7 @@
 const http=require('http');
 const fs=require('fs');
 const url=require('url');
-const replaceTemplate=(temp,product)=>{
-let output=temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-output=output.replace(/{%IMAGE%}/g,product.image);
-output=output.replace(/{%PRICE%}/g,product.price);
-output=output.replace(/{%FROM%}/g,product.from);
-output=output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-output=output.replace(/{%QUANTITY%}/g,product.quantity);
-output=output.replace(/{%DESCRIPTION%}/g,product.description);
-output=output.replace(/{%ID%}/g,product.id);
-if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
-return output;
-}
+const replaceTemplate=require('./module/replacetemp')
 const tempOverview=fs.readFileSync('./templates/overview.html','utf-8');
 const tempCard=fs.readFileSync('./templates/template-card.html','utf-8');
 const tempProduct=fs.readFileSync('./templates/product.html','utf-8');
